648325f6f837b3686a3719ef360733b3f2adbd26
shaikjani-16 <106083984+shaikjani-16@users.noreply.github.com>
2024-07-27 23:10:06 +0530

    task final

--- server/posts/posts.router.js
+++ server/posts/posts.router.js
@@ -8,49 +8,53 @@ const router = express.Router();
 router.get('/', async (req, res) => {
   try {
     const { start, limit } = req.query;
-    console.log('start in router', start, 'limit in router', limit);
 
     const posts = await fetchPosts({
       start: parseInt(start),
       limit: parseInt(limit),
     });
-    const postsWithImagesAndUsers = await Promise.all(
-      posts.map(async post => {
-        try {
-          const photosResponse = await axios.get(
-            `https://jsonplaceholder.typicode.com/albums/${post.id}/photos`,
-          );
-          const photos = photosResponse.data;
 
-          const user = await fetchUserById(post.userId);
+    const Post_Images = async post => {
+      try {
+        const photosResponse = await axios.get(
+          `https://jsonplaceholder.typicode.com/albums/${post.id}/photos`,
+        );
+        const photos = photosResponse.data;
 
-          const userName = user ? user.user.name : 'Unknown';
-          const email = user ? user.user.email : 'Unknown';
+        const user = await fetchUserById(post.userId);
+        const userName = user ? user.user.name : 'Unknown';
+        const email = user ? user.user.email : 'Unknown';
 
-          return {
-            ...post,
-            images: photos.map(photo => ({ url: photo.url })),
-            userName,
-            email,
-          };
-        } catch (error) {
-          console.error(
-            `Error fetching photos or user for post ${post.id}:`,
-            error,
-          );
-          return {
-            ...post,
-            images: [],
-            userName: 'Unknown',
-            userEmail: 'Unknown',
-          };
-        }
-      }),
-    );
+        return {
+          ...post,
+          images: photos.map(photo => ({ url: photo.url })),
+          userName,
+          email,
+        };
+      } catch (error) {
+        console.error(
+          `Error fetching photos or user for post ${post.id}:`,
+          error,
+        );
 
-    res.json(postsWithImagesAndUsers);
+        return {
+          ...post,
+          images: [],
+          userName: 'Unknown',
+          email: 'Unknown',
+        };
+      }
+    };
+
+    const postsWithImages = [];
+    for (const post of posts) {
+      const completedPost = await Post_Images(post);
+      postsWithImages.push(completedPost);
+    }
+
+    res.json(postsWithImages);
   } catch (error) {
-    console.error('Error fetching posts or photos:', error);
+    console.error('Error fetching images:', error);
     res.status(500).send('Server error');
   }
 });
